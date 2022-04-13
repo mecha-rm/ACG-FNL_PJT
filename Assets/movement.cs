8e7ae0679cb886929610e2c58516acb172fb072d
Eric Aivaliotis <eaivaliotis@hotmail.com>
2022-04-12 23:34:02 -0400

    Eric's push
    this is at the moment a test push

---
+++ Assets/movement.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Translate(10 * Input.GetAxis("Horizontal") * Time.deltaTime, 0f, 0f); 
+
+    }
+}
